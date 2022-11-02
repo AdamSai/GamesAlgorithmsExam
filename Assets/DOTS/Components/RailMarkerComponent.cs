a37b182721bd240cf17a8c8f0b396e83e6fa9ff2
Adam Saidane <adamsaidane252@gmail.com>
2022-11-02 14:43:03 +0100

    RailMarkerAuthoring

---
+++ Assets/DOTS/Components/RailMarkerComponent.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+
+namespace DOTS.Components
+{
+    public enum RailMarkerTypes : byte
+    {
+        NONE,
+        PLATFORM_START,
+        PLATFORM_END,
+    }
+    
+    public struct RailMarkerComponent : IComponentData
+    {
+        public byte MetroLineID;
+        public byte PointIndex;
+        public RailMarkerTypes RailMarkerType;
+    }
+}
--- Assets/src/Metro.cs
+++ Assets/src/Metro.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Collections;
 using UnityEditor;
 using UnityEngine;
 
